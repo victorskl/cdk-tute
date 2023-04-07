305dc20e5386416ae443cfbd06ea877e7b921319
Victor San Kho Lin <victor@sankholin.com>
2023-04-07 20:59:17 +1000

    Updated boiler-app with v2

--- boiler-app/boiler_app/boiler_app_stack.py
+++ boiler-app/boiler_app/boiler_app_stack.py
@@ -1,9 +1,20 @@
-from aws_cdk import core
+from aws_cdk import (
+    Duration,
+    Stack,
+    aws_sqs as sqs,
+)
+from constructs import Construct
 
 
-class BoilerAppStack(core.Stack):
+class BoilerAppStack(Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        # example resource
+        queue = sqs.Queue(
+            self, "BoilerAppQueue",
+            visibility_timeout=Duration.seconds(300),
+        )
